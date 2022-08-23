e81fdd7a26fb3b21a86d9ff78d159b858b07fb15
Priscah Orori <ororipriscah@gmail.com>
2022-08-22 18:38:16 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,31 @@
 #include "main.h"
 
 /**
- * create_file - create a file
- * @filename: filename
- * @text_content: data to place on file
- * Return: Success ? 1 : -1
+ * create_file - creates a file and add content to it
+ * @filename: file name
+ * @text_content: content to put in the file
+ *
+ * Return: 1 for success or -1 if it fails
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int file_desc, _write;
+	int fd, i = 0, retval = 1;
 
 	if (!filename)
 		return (-1);
-
-	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file_desc == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-
 	if (!text_content)
 	{
-		close(file_desc);
+		close(fd);
 		return (1);
 	}
-	while (text_content[slen(text_content)])
-		slen(text_content)++;
-
-	_write = write(file_desc, text_content, slen(text_content));
-	if (_write == -1 || _write != slen(text_content))
-	{
-		close(file_desc);
+	while (text_content[i])
+		i++;
+	retval = write(fd, text_content, i);
+	close(fd);
+	if (retval == -1 || retval != i)
 		return (-1);
-	}
-	close(file_desc);
 	return (1);
 }
