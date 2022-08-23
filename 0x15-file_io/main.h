d349d5c643b648acf380c71f8715b1ff2d94c956
Priscah Orori <ororipriscah@gmail.com>
2022-08-22 18:35:35 -0700

    Corrections on main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,53 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+__home int slen(char *s)
+{
+	if (!*s)
+		return (0);
+	s++;
+	return (1 + slen(s));
+}
+
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+
+__home char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @file_desc: The file descriptor to be closed.
+ */
+
+__home void close_file(int file_desc)
+{
+	int c;
+
+	c = close(file_desc);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 
 #endif
